dc06bd6720a7c8d2671e04113611fca3ab34fd24
David <zdavid1@myges.fr>
2023-06-11 16:29:22 +0200

    services

--- rent-api-properties/src/main/java/fr/esgi/rent/api/RentalPropertyResource.java
+++ rent-api-properties/src/main/java/fr/esgi/rent/api/RentalPropertyResource.java
@@ -1,5 +1,6 @@
 package fr.esgi.rent.api;
 
+import fr.esgi.rent.domain.RentalPropertyService;
 import fr.esgi.rent.dto.request.RentalPropertyRequestPatchDto;
 import fr.esgi.rent.entity.RentalPropertyEntity;
 import fr.esgi.rent.dto.request.RentalPropertyRequestDto;
@@ -7,87 +8,63 @@ import fr.esgi.rent.dto.response.RentalPropertyResponseDto;
 import fr.esgi.rent.mapper.RentalPropertyDtoMapper;
 import fr.esgi.rent.repository.RentalPropertyRepository;
 import jakarta.validation.Valid;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
+
+
 
 import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.NO_CONTENT;
 
 @RestController
 @RequestMapping("/rent-properties-api")
 public class RentalPropertyResource {
-
-    private final RentalPropertyRepository rentalPropertyRepository;
-    private final RentalPropertyDtoMapper rentalPropertyDtoMapper;
+    private final RentalPropertyService rentalPropertyService;
 
     public RentalPropertyResource(RentalPropertyRepository rentalPropertyRepository,
                                   RentalPropertyDtoMapper rentalPropertyDtoMapper) {
-        this.rentalPropertyRepository = rentalPropertyRepository;
-        this.rentalPropertyDtoMapper = rentalPropertyDtoMapper;
+        this.rentalPropertyService = new RentalPropertyService(rentalPropertyRepository, rentalPropertyDtoMapper);
     }
 
     @GetMapping("/rental-properties")
     public List<RentalPropertyResponseDto> getRentalProperties() {
-        List<RentalPropertyEntity> rentalProperties = rentalPropertyRepository.findAll();
-
-        return rentalPropertyDtoMapper.mapToDtoList(rentalProperties);
+        return rentalPropertyService.getRentalProperties();
     }
 
     @GetMapping("/rental-properties/{id}")
     public RentalPropertyResponseDto getRentalPropertyById(@PathVariable String id) {
-        Optional<RentalPropertyResponseDto> optRentalPropertyDtoResponse = rentalPropertyRepository.findById(Integer.parseInt(id))
-                .map(rentalPropertyDtoMapper::mapToDto);
-
-        if (optRentalPropertyDtoResponse.isEmpty()) {
+        try {
+            return rentalPropertyService.getRentalPropertyById(id);
+        } catch (Exception e) {
             throw new ResponseStatusException(
                     HttpStatus.NOT_FOUND
             );
         }
-        return optRentalPropertyDtoResponse.get();
-
     }
 
     @PostMapping("/rental-properties")
     @ResponseStatus(CREATED)
     public void createRentalProperty(@Valid @RequestBody RentalPropertyRequestDto rentalPropertyRequestDto) {
-        RentalPropertyEntity rentalPropertyEntity = rentalPropertyDtoMapper.mapToEntity(rentalPropertyRequestDto);
-        rentalPropertyRepository.save(rentalPropertyEntity);
+        rentalPropertyService.createRentalProperty(rentalPropertyRequestDto);
     }
 
     @PutMapping("/rental-properties/{id}")
     public void updateRentalProperty(@PathVariable String id, @Valid @RequestBody RentalPropertyRequestDto rentalPropertyRequestDto) {
-        Optional<RentalPropertyEntity> optRentalPropertyEntity = rentalPropertyRepository.findById(Integer.parseInt(id));
-        RentalPropertyEntity rentalPropertyEntity = rentalPropertyDtoMapper.mapToEntity(rentalPropertyRequestDto);
-        if (optRentalPropertyEntity.isPresent()) {
-            rentalPropertyEntity.setId(Integer.parseInt(id));
-        }
-        rentalPropertyRepository.save(rentalPropertyEntity);
+        rentalPropertyService.updateRentalProperty(id, rentalPropertyRequestDto);
     }
 
     @DeleteMapping("/rental-properties/{id}")
+    @ResponseStatus(NO_CONTENT)
     public void deleteRentalProperty(@PathVariable String id) {
-        Optional<RentalPropertyEntity> optRentalPropertyEntity = rentalPropertyRepository.findById(Integer.parseInt(id));
-        if (optRentalPropertyEntity.isPresent()) {
-            rentalPropertyRepository.deleteById(Integer.parseInt(id));
-        }
+        rentalPropertyService.deleteRentalProperty(id);
     }
 
-    //patch only what is not null
     @PatchMapping("/rental-properties/{id}")
     public void patchRentalProperty(@PathVariable String id, @Valid @RequestBody RentalPropertyRequestPatchDto rentalPropertyRequestPatchDto) {
-        Optional<RentalPropertyEntity> optRentalPropertyEntity = rentalPropertyRepository.findById(Integer.parseInt(id));
-        if (optRentalPropertyEntity.isPresent()) {
-            RentalPropertyEntity rentalPropertyEntity = optRentalPropertyEntity.get();
-            rentalPropertyEntity.setRentAmount(rentalPropertyRequestPatchDto.rentAmount());
-            rentalPropertyRepository.save(rentalPropertyEntity);
-        } else {
-            throw new ResponseStatusException(
-                    HttpStatus.NOT_FOUND
-            );
-        }
+        rentalPropertyService.patchRentalProperty(id, rentalPropertyRequestPatchDto);
     }
 }
